ce7e56c0f6ba9ec11a6417c9925dcdcab003b966
unknown <20170493@ricaldone.edu.sv>
2019-03-17 14:15:01 -0600

    hola

--- Expo2019/core/helpers/public_page.php
+++ Expo2019/core/helpers/public_page.php
@@ -3,4 +3,7 @@ la vida no vale nada
 callate que me desesperas
 
 Bryan se la come y viene vestido para fiesta rosa
+
+Bryan es muy guapo y viene vestido para conocer al amor de su variant_date_from_timestamp
+
 hola soy bryan

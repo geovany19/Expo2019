fea99ec42506d59b0ce3b6ce51806addcbed6c7e
Geovany Fuentes <20160717@ricaldone.edu.sv>
2019-03-17 16:13:36 -0600

    cambio

--- Expo2019/views/dashboard/index.php
+++ Expo2019/views/dashboard/index.php
@@ -1,27 +1,4 @@
 <?php
-            include('../../core/helpers/dashboard/navbaradmin.php');
-            include('../../core/helpers/dashboard/footeradmin.php');
+include "../../core/helpers/dashboard/public_page.php";
+Public_page::header("Principal");
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/png" href="../../resources/img/jardin-iso.png">
-    <link rel="stylesheet" type="text/css" href="../../resources/css/dashboard/estilos_admin.css">
-    <link rel="stylesheet" href="../../resources/css/materialize.css">
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <title>Administrador</title>
-</head>
-<body class="has-fixed-sidenav">
-    <header>
-        <?php
-            echo navbar :: nav();
-            echo navbar :: sidenav();
-
-        ?>
-    </header>
-</body>
-</html>

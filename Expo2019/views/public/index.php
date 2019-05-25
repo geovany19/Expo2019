a203f5b520e2d0989259ce0d02a31335a585b560
ElBatmanBosco <bryaleama@gmail.com>
2019-05-25 13:30:01 -0600

    public

--- Expo2019/views/public/index.php
+++ Expo2019/views/public/index.php
@@ -1,41 +1,43 @@
 <?php
-include "../../core/helpers/public/public_helper.php";
-public_helper::head("Iniciar sesión");
-public_helper::navIndex();
+include "../../core/helpers/public/public.php";
+public::head("Iniciar sesión");
 ?>
-<main class="main">
-	<div class="login-form">
-		<form action="../../views/dashboard/pagina.php" method="post">
-			<div class="avatar">
-				<img src="../../resources/img/dashboard/img4.jpg" class="rounded-circle" alt="Avatar" width="30" height="65">
-			</div>
-			<h2 class="text-center">Iniciar sesión</h2>
-			<div class="form-group">
-				<input type="text" class="form-control" name="username" placeholder="Nombre de usuario" required="required">
-			</div>
-			<div class="form-group">
-				<input type="password" class="form-control" name="password" placeholder="Contraseña" required="required">
-			</div>
-			<div class="form-group">
-				<button type="submit" class="btn btn-primary btn-lg btn-block">Iniciar sesión</button>
-			</div>
-			<div class="clearfix">
-				<label class="pull-left checkbox-inline"><input type="checkbox"> Recuerdame</label>
-				<a href="#" class="pull-right">¿Olvidaste tu contraseña?</a>
-			</div>
-		</form>
-		<p class="text-center small">¿No estás registrado? <a href="#">¡Registrate aquí!</a></p>
-	</div>
-</main>
-<footer>
 
-</footer>
-<script type="text/javascript" src="../../resources/js/jquery-3.3.1.min.js"></script>
-<script type="text/javascript" src="../../resources/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="../../resources/js/popper.min.js"></script>
-<script type="text/javascript" src="../../resources/js/sidenav.js"></script>
-<script type="text/javascript" src="../../resources/js/Chart.js"></script>
-<script type="text/javascript" src="../../resources/js/Chart.bundle.js"></script>
+<body id="body">
+	<main id="main">
+		<div class="row">
+			<div class="col col-sm-12">
+				<div class="login-form">
+					<form action="../../views/dashboard/pagina.php" method="post">
+						<div class="avatar">
+							<img src="../../resources/img/dashboard/img4.jpg" class="rounded-circle" alt="Avatar" width="30" height="65">
+						</div>
+						<h2 class="text-center">Iniciar sesión</h2>
+						<div class="form-group">
+							<input type="text" class="form-control" name="username" placeholder="Nombre de usuario" required="required">
+						</div>
+						<div class="form-group">
+							<input type="password" class="form-control" name="password" placeholder="Contraseña" required="required">
+						</div>
+						<div class="form-group">
+							<button type="submit" class="btn btn-primary btn-lg btn-block">Iniciar sesión</button>
+						</div>
+						<div class="clearfix">
+							<label class="pull-left checkbox-inline"><input type="checkbox"> Recuerdame</label>
+							<a href="#" class="pull-right">¿Olvidaste tu contraseña?</a>
+						</div>
+					</form>
+					<p class="text-center small">¿No estás registrado? <a href="#">¡Registrate aquí!</a></p>
+				</div>
+			</div>
+		</div>
+	</main>
+	<script type="text/javascript" src="../../resources/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="../../resources/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../../resources/js/popper.min.js"></script>
+	<script type="text/javascript" src="../../resources/js/sidenav.js"></script>
+	<script type="text/javascript" src="../../resources/js/Chart.js"></script>
+	<script type="text/javascript" src="../../resources/js/Chart.bundle.js"></script>
 </body>
 
 </html>

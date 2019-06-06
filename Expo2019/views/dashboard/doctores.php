bb187579104f7a53b2083b4b3cb16a230bcfb257
federamirez00 <20140050@ricaldone.edu.sv>
2019-06-06 14:36:30 -0600

    cambio en doctores.php

--- Expo2019/views/dashboard/doctores.php
+++ Expo2019/views/dashboard/doctores.php
@@ -1,29 +1,29 @@
 <?php
 include "../../core/helpers/dashboard_helper.php";
 dashboard_helper::head("Doctores");
-dashboard_helper::nav() ;
+dashboard_helper::nav();
 ?>
 <main>
-    <h1 class="text-center">Doctores</h1>
-    <div>
-        <table class="table table-responsive table-hover"  id="tabla-doctores">
-            <thead class="thead-dark">
-                <!--Agregando los campos fijos a la tabla-->
-                <tr>
-                    <th scope="col">Código</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Apellido</th>
-                    <th scope="col">Correo</th>
-                    <th scope="col">Nombre de usuario</th>
-                    <th scope="col">Fecha de nacimiento</th>
-                    <th scope="col">Foto</th>
-                    <th scope="col">Estado</th>
-                    <th scope="col">Acciones</th>
-                </tr>
-            </thead>
-            <tbody id="table-body"></tbody>
-        </table>
-    </div>
+	<h1 class="text-center">Doctores</h1>
+	<div>
+		<table class="table table-responsive table-hover" id="tabla-doctores">
+			<thead class="thead-dark">
+				<!--Agregando los campos fijos a la tabla-->
+				<tr>
+					<th scope="col">Código</th>
+					<th scope="col">Nombre</th>
+					<th scope="col">Apellido</th>
+					<th scope="col">Correo</th>
+					<th scope="col">Nombre de usuario</th>
+					<th scope="col">Fecha de nacimiento</th>
+					<th scope="col">Foto</th>
+					<th scope="col">Estado</th>
+					<th scope="col">Acciones</th>
+				</tr>
+			</thead>
+			<tbody id="table-body"></tbody>
+		</table>
+	</div>
 </main>
 <!-- Ventana para modificar un registro existente -->
 <div class="modal fade" id="modal-update" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
@@ -33,12 +33,12 @@ dashboard_helper::nav() ;
 				<h5 class="modal-title" id="exampleModalLongTitle">Actualizar Doctor</h5>
 			</div>
 			<div class="modal-body">
-			<form id="form-update" enctype="multipart/form-data">
-					<input type="hidden" id="id_doctor" name="id_doctor" >
-					<input type="text" id="foto_doctor" name="foto_doctor" >
+				<form id="form-update" enctype="multipart/form-data">
+					<input type="hidden" id="id_doctor" name="id_doctor">
+					<input type="text" id="foto_doctor" name="foto_doctor">
 					<div class="form-group">
 						<label for="update_nombre">Nombre:</label>
-						<input type="text" class="form-control"  class="form-control is-valid" id="update_nombre" name="update_nombre" aria-describedby="emailHelp" placeholder="Nombre:">
+						<input type="text" class="form-control" class="form-control is-valid" id="update_nombre" name="update_nombre" aria-describedby="emailHelp" placeholder="Nombre:">
 					</div>
 					<div class="form-group">
 						<label for="update_apellido">Apellido:</label>
@@ -61,23 +61,23 @@ dashboard_helper::nav() ;
 						<input type="file" id="update_archivo" name="update_archivo" class="file-input">
 					</div>
 					<div class="col s12 m6">
-										<p>
-												<div class="checkbox">
-														<span>Estado:</span>
-														<label>
-																<i class="material-icons">visibility_off</i>
-																<input id="create_estado"  type="checkbox" data-toggle="toggle" checked/>
-																<span class="lever"></span>
-																<i class="material-icons">visibility</i>
-														</label>
-												</div>
-										</p>
-								</div>
-	<div class="modal-footer justify-content-center aling-items-center">
-				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-				<button type="submit" class="btn btn-primary">Actualizar</button>
-			</div>
-</form>
+						<p>
+							<div class="checkbox">
+								<span>Estado:</span>
+								<label>
+									<i class="material-icons">visibility_off</i>
+									<input id="create_estado" type="checkbox" data-toggle="toggle" checked />
+									<span class="lever"></span>
+									<i class="material-icons">visibility</i>
+								</label>
+							</div>
+						</p>
+					</div>
+					<div class="modal-footer justify-content-center aling-items-center">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button type="submit" class="btn btn-primary">Actualizar</button>
+					</div>
+				</form>
 			</div>
 
 		</div>
